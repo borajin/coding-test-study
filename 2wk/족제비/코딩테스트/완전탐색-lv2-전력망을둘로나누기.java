1bba31b4900d6cda8a03ed04877847b59917a439
진보라 (Jin Bora) <55897222+borajin@users.noreply.github.com>
2023-10-10 13:55:39 +0900

    Update 완전탐색-lv2-전력망을둘로나누기.java

--- 2wk/족제비/코딩테스트/완전탐색-lv2-전력망을둘로나누기.java
+++ 2wk/족제비/코딩테스트/완전탐색-lv2-전력망을둘로나누기.java
@@ -11,21 +11,40 @@ class Solution {
         // wires 를 전부 탐색하여, n번째 wire 가 끊겼을 때
         // n번째 간선의 양 노드 각각의 하위 노드 개수 차이 중
         // 가장 적은 값을 return
+
+        /*
+        테스트 1 〉	통과 (9.65ms, 77.4MB)
+        테스트 2 〉	통과 (6.47ms, 79.5MB)
+        테스트 3 〉	통과 (6.37ms, 66.3MB)
+        테스트 4 〉	통과 (6.23ms, 72.7MB)
+        테스트 5 〉	통과 (7.44ms, 70.2MB)
+        테스트 6 〉	통과 (0.38ms, 78.3MB)
+        테스트 7 〉	통과 (0.45ms, 83MB)
+        테스트 8 〉	통과 (1.46ms, 90.8MB)
+        테스트 9 〉	통과 (1.51ms, 73.7MB)
+        테스트 10 〉	통과 (9.67ms, 74.8MB)
+        테스트 11 〉	통과 (17.88ms, 84.9MB)
+        테스트 12 〉	통과 (6.54ms, 83.5MB)
+        테스트 13 〉	통과 (7.67ms, 79.5MB)
+        */
         
         List<LinkedList<Integer>> towerTree = makeTowerTree(n, wires);
 
         int answer = 999;
         for (int[] wire : wires) {
-          towerTree.get(wire[0]-1).remove(Integer.valueOf(wire[1]-1));
-          towerTree.get(wire[1]-1).remove(Integer.valueOf(wire[0]-1));
+          int leftNode = wire[0]-1;
+          int rightNode = wire[1]-1;
+            
+          towerTree.get(leftNode).remove(Integer.valueOf(rightNode));
+          towerTree.get(rightNode).remove(Integer.valueOf(leftNode));
 
-          int first = getConnectedTowerCountByBfs(towerTree, wire[0]-1);
-          int second = getConnectedTowerCountByBfs(towerTree, wire[1]-1);
+          int first = getConnectedTowerCountByBfs(towerTree, leftNode);
+          int second = getConnectedTowerCountByBfs(towerTree, rightNode);
 
           answer = Math.min(answer, Math.abs(first - second));
 
-          towerTree.get(wire[0]-1).add(wire[1]-1);
-          towerTree.get(wire[1]-1).add(wire[0]-1);
+          towerTree.get(leftNode).add(rightNode);
+          towerTree.get(rightNode).add(leftNode);
         }
         
         return answer;
@@ -64,8 +83,11 @@ class Solution {
         }
 
         for (int[] wire : wires) {
-          towerTree.get(wire[0] - 1).add(wire[1] - 1);
-          towerTree.get(wire[1] - 1).add(wire[0] - 1);
+          int leftNode = wire[0]-1;
+          int rightNode = wire[1]-1;
+            
+          towerTree.get(leftNode).add(rightNode);
+          towerTree.get(rightNode).add(leftNode);
         }
 
         return towerTree;
