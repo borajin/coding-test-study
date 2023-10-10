83397b862ee50d9abb1c0aa8f94ad894818e098c
gyeonghyun kim <gyeonghyun.kimm@gmail.com>
2023-10-10 21:49:46 +0900

    [2wk] 전력망을 둘로 나누기 코드 수정

--- 2wk/버즈/coding-test/전력망을둘로나누기.java
+++ 2wk/버즈/coding-test/전력망을둘로나누기.java
@@ -1,22 +1,87 @@
+import java.util.LinkedList;
+
 class Solution {
-    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        long answer = 0;
-        int sumDel = 0;
-        int sumPick = 0;
-       
-        for (int i = n - 1; i >= 0; i--) {
-            int cnt = 0;
-            sumDel += deliveries[i];
-            sumPick += pickups[i];
-            
-            while (sumDel > 0 || sumPick > 0) {
-                sumDel -= cap;
-                sumPick -= cap;
-                cnt++;
-            }
-            answer += (i + 1) * 2 * cnt;
+    public int solution(int n, int[][] wires) {
+        int answer = Integer.MAX_VALUE;
+        LinkedList<LinkedList<Integer>> graph = new LinkedList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new LinkedList<Integer>());
         }
+        for (int[] wire : wires) {
+            graph.get(wire[0]).add(wire[1]);
+            graph.get(wire[1]).add(wire[0]);
+        }
+        for (int[] wire : wires) {
+            boolean[] visited = new boolean[n + 1];
+            graph.get(wire[0]).remove(Integer.valueOf(wire[1]));
+            graph.get(wire[1]).remove(Integer.valueOf(wire[0]));
+            int net1 = bfs(graph, wire[0], visited);
+            int net2 = bfs(graph, wire[1], visited);
+            answer = Math.min(answer, Math.abs(net1 - net2));
+            graph.get(wire[0]).add(wire[1]);
+            graph.get(wire[1]).add(wire[0]);
+        }
+        
+        
         return answer;
     }
+    
+    private int bfs(LinkedList<LinkedList<Integer>> graph, int start, boolean[] visited) {
+        LinkedList<Integer> q = new LinkedList<>();
+        q.add(start);
+        visited[start] = true;
+        int cnt = 0;
+        while (q.peek() != null) {
+            int curr = q.poll();
+            cnt++;
+            for (int next : graph.get(curr)) {
+                if (!visited[next]) {
+                    q.add(next);
+                    visited[next] = true;
+                }
+            }
+        }
+        return cnt;
+    } 
+    
+//     public int solution(int n, int[][] wires) {
+//         int answer = Integer.MAX_VALUE;
+//         int[][] graph = new int[n + 1][n + 1];
+//         for (int[] wire : wires) {
+//             graph[wire[0]][wire[1]] = 1;
+//             graph[wire[1]][wire[0]] = 1;
+//         }
+//         for (int[] wire : wires) {
+//             boolean[] visited = new boolean[n + 1];
+//             graph[wire[0]][wire[1]] = 0;
+//             graph[wire[1]][wire[0]] = 0;
+//             int net1 = bfs(graph, wire[0], visited);
+//             int net2 = bfs(graph, wire[1], visited);
+//             answer = Math.min(answer, Math.abs(net1 - net2));
+//             graph[wire[0]][wire[1]] = 1;
+//             graph[wire[1]][wire[0]] = 1;
+//         }
+        
+        
+//         return answer;
+//     }
+    
+//     private int bfs(int[][] graph, int start, boolean[] visited) {
+//         LinkedList<Integer> q = new LinkedList<>();
+//         q.add(start);
+//         visited[start] = true;
+//         int cnt = 0;
+//         while (q.peek() != null) {
+//             int curr = q.poll();
+//             cnt++;
+//             for (int i = 0; i < graph.length; i++) {
+//                 if (graph[curr][i] == 1 && !visited[i]) {
+//                     q.add(i);
+//                     visited[i] = true;
+//                 }
+//             }
+//         }
+//         return cnt;
+//     } 
 }
 
