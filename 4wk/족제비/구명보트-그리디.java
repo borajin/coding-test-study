24d866eff22c003413dcc4d93f6a0346dbb41757
진보라 (Jin Bora) <55897222+borajin@users.noreply.github.com>
2023-10-26 14:03:31 +0900

    Update 구명보트-그리디.java

--- 4wk/족제비/구명보트-그리디.java
+++ 4wk/족제비/구명보트-그리디.java
@@ -1,25 +1,30 @@
 import java.util.Arrays;
 
-class Solution {
-    public int solution(int[] people, int limit) {
-        int answer = 0;
-        
-        Arrays.sort(people);
-        
-        
-        //가장 무거운 사람 <> 가장 가벼운 사람 끼리 태우기
-        //가장 무거운 사람이 무게 초과하면 혼자 태워 보내기
-        
-        int lightIndex=0;
-        for(int heavyIndex = people.length - 1; heavyIndex >= lightIndex; heavyIndex--) {
-          if(people[heavyIndex] + people[lightIndex] <= limit) {
-            lightIndex++;
-            answer++;
-          } else {
-            answer++;
-          }
-        }
-        
-        return answer;
+public class 구명보트 {
+  
+  //https://school.programmers.co.kr/learn/courses/30/lessons/42885
+  
+  public static void main(String[] args) {
+    int[] people = {70, 50, 80, 50};
+    int limit = 100;
+
+    //정원2명 : 가장 무거운 사람 - 가장 가벼운 사람 끼리 태우기
+    //가장 무거운 사람이 무게 초과하면 혼자 태워 보내기
+
+    int answer = 0;
+
+    Arrays.sort(people);
+
+    int lightIndex=0;
+    for(int heavyIndex = people.length - 1; heavyIndex >= lightIndex; heavyIndex--) {
+      if(people[heavyIndex] + people[lightIndex] <= limit) {
+        lightIndex++;
+        answer++;
+      } else {
+        answer++;
+      }
     }
+
+    System.out.println(answer);
+  }
 }
