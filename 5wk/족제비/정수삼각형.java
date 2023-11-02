f6317da3580396f5827415e835b04be9db690b3e
진보라 (Jin Bora) <55897222+borajin@users.noreply.github.com>
2023-11-02 23:46:00 +0900

    Update 정수삼각형.java

--- 5wk/족제비/정수삼각형.java
+++ 5wk/족제비/정수삼각형.java
@@ -5,6 +5,7 @@ public class 정수삼각형 {
   public static void main(String[] args) {
     int[][] triangle = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
 
+    //마지막 줄은 그 자체로 최대값이므로 제외 (length-2)
     for (int i=triangle.length-2; i>=0; i--) {
       for (int j=0; j<triangle[i].length; j++) {
         triangle[i][j] = Math.max(triangle[i][j] + triangle[i+1][j], triangle[i][j] + triangle[i+1][j+1]);
